32b96af3becf692a82d1a91592e97bdbd32bae19
Safiquddin <safiquddinkhan@hotmail.com>
2023-09-19 15:35:11 +0530

    added backend in root

--- modules/vnetwork/main.tf
+++ modules/vnetwork/main.tf
@@ -24,6 +24,7 @@ resource "azurerm_public_ip" "public" {
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
+
 # Create Network Security Group and rules
 resource "azurerm_network_security_group" "test_nsg" {
   name                = "test-nsg"
