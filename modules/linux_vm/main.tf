6730be89d36d833c4536793577ac7082c22d605b
Safiquddin <safiquddinkhan@hotmail.com>
2023-09-19 15:13:28 +0530

    updated pem file path

--- modules/linux_vm/main.tf
+++ modules/linux_vm/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   # }
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("~/.ssh/azurekey.pub")
+    public_key = file("azurekey.pub")
   }
   os_disk {
     caching              = "ReadWrite"
