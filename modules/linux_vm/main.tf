4800091e2e510c1437c2ea1c1aebc23f976c8325
Safiquddin Khan <84470622+safiqkhan@users.noreply.github.com>
2023-09-20 23:43:12 +0530

    Update main.tf

--- modules/linux_vm/main.tf
+++ modules/linux_vm/main.tf
@@ -1,11 +1,11 @@
- resource "tls_private_key" "key" {
-   algorithm   = "RSA"
-   rsa_bits    = 4096
- }
- resource "local_file" "azurekey" {
-   filename = "azurekey.pem"
-   content = tls_private_key.key.private_key_pem
- }
+resource "tls_private_key" "key" {
+  algorithm   = "RSA"
+  rsa_bits    = 4096
+}
+resource "local_file" "azurekey" {
+  filename = "azurekey.pem"
+  content = tls_private_key.key.private_key_pem
+}
 resource "azurerm_linux_virtual_machine" "vm" {
   name                  = var.vm_name
   location              = var.location
@@ -41,20 +41,20 @@ resource "azurerm_linux_virtual_machine" "vm" {
   #   sku       = "8-lvm-gen2"
   #   version   = "latest"
   # }
-   provisioner "file" {
+  provisioner "file" {
     source      = "customdata.sh"    # Local path to your script
     destination = "/tmp/script.sh"  # Destination path on the VM
   }
-   provisioner "remote-exec" {
-     inline = [
-       "chmod +x /tmp/customdata.sh",
-       "/tmp/customdata.sh > /tmp/log.txt 2>&1",  # Run the script and capture output in log
-     ]
-   }
-   connection {
-     type        = "ssh"
-     host        = self.public_ip_address
-     user        = var.admin_username
-     private_key = tls_private_key.key.private_key_pem
-   }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "/tmp/script.sh > /tmp/log.txt 2>&1",  # Run the script and capture output in log
+    ]
+  }
+  connection {
+    type        = "ssh"
+    host        = self.public_ip_address
+    user        = var.admin_username
+    private_key = tls_private_key.key.private_key_pem
+  }
 }
