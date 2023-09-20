6de434c996af6177ac46b83fd11d403a1f5780d9
Safiquddin Khan <80240469+Safiquddin@users.noreply.github.com>
2023-09-20 23:35:43 +0530

    Update main.tf

--- modules/linux_vm/main.tf
+++ modules/linux_vm/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   # size                  = "Standard_B1s"
   size                  = "Standard_F4s"
   admin_username = var.admin_username
-  custom_data = filebase64("customdata.sh")
+  // custom_data = filebase64("customdata.sh")
   # review cloud-init logs at /var/log/cloud-init.log
   admin_ssh_key {
      username   = var.admin_username
@@ -41,21 +41,20 @@ resource "azurerm_linux_virtual_machine" "vm" {
   #   sku       = "8-lvm-gen2"
   #   version   = "latest"
   # }
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "sudo yum install -y curl policycoreutils-python openssh-server 2>&1 /dev/null",
-  #     "curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash",
-  #     "public_ip=$(curl ifcfg.me 2> /dev/null)",
-  #     "sudo EXTERNAL_URL=http://$public_ip yum install -y gitlab-ce",
-  #     "curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh | sudo bash",
-  #     "sudo yum install -y gitlab-runner",
-  #     "sudo systemctl enable gitlab-runner",
-  #   ]
-  # }
-  # connection {
-  #   type        = "ssh"
-  #   host        = self.public_ip_address
-  #   user        = var.admin_username
-  #   private_key = tls_private_key.key.private_key_pem
-  # }
+   provisioner "file" {
+    source      = "customdata.sh"    # Local path to your script
+    destination = "/tmp/script.sh"  # Destination path on the VM
+  }
+   provisioner "remote-exec" {
+     inline = [
+       "chmod +x /tmp/customdata.sh",
+       "/tmp/customdata.sh > /tmp/log.txt 2>&1",  # Run the script and capture output in log
+     ]
+   }
+   connection {
+     type        = "ssh"
+     host        = self.public_ip_address
+     user        = var.admin_username
+     private_key = tls_private_key.key.private_key_pem
+   }
 }
