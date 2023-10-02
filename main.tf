2fa5cb7cc415fa4d27162a0eb9cb58b819ad5779
Safiquddin Khan <80240469+Safiquddin@users.noreply.github.com>
2023-10-02 21:11:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
+  // subscription_id = var.ARM_SUBSCRIPTION_ID
+  // client_id       = var.ARM_CLIENT_ID
+  // client_secret   = var.ARM_CLIENT_SECRET
+  // tenant_id       = var.ARM_TENANT_ID
 }
 
 resource "azurerm_resource_group" "rg" {
